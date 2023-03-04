ef85844d852d65193d7ebf5d354181d6c128a660
Kirill <kominov.kirill@yandex.ru>
2023-03-04 22:55:04 +0500

    Finish homework6

---
+++ HomeWorks/homework6/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+
+int GetNum(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int[] getArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = GetNum($"Введите {i + 1}-й элемент массива");
+    }
+    return array;
+}
+
+int countPositive(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = GetNum("Сколько элементов будет в массиве? ");
+int[] Array = getArray(length);
+Console.WriteLine("[ " + String.Join(", ", Array) + " ]");
+int positiveNumbers = countPositive(Array);
+Console.WriteLine($"Вы ввели {positiveNumbers} положительных чисел");
---
+++ HomeWorks/homework6/Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.Clear();
+int line1 = 1;
+int line2 = 2;
+
+int GetNum(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+double[] getArray(int lineNum)
+{
+    double[] arrayLine = new double[2];
+    arrayLine[0] = GetNum($"Введите k для {lineNum} прямой");
+    arrayLine[1] = GetNum($"Введите b для {lineNum} прямой");
+
+    return arrayLine;
+}
+double[] KB1 = getArray(line1);
+double[] KB2 = getArray(line2);
+Console.WriteLine("[ " + String.Join(", ", KB1) + " ]");
+Console.WriteLine("[ " + String.Join(", ", KB2) + " ]");
+
+if (ValidateLines(KB1, KB2))
+{
+    double[] coords = findCoords(KB1, KB2);
+    Console.Write($"Точка пересечения уравнений: y = {KB1[0]} * x + {KB1[1]} и y = {KB2[0]} * x + {KB2[1]} ");
+    Console.WriteLine($" имеет координаты: ({coords[0]}, {coords[1]})");
+}
+
+double[] findCoords(double[] arrayNum1, double[] arrayNum2)
+{
+    double[] coords = new double[2];
+    coords[0] = (KB1[0] - KB2[0]) / (KB2[1] - KB1[1]);
+    coords[1] = KB1[0] * coords[0] + KB1[1];
+    return coords;
+}
+
+bool ValidateLines(double[] arrayNum1, double[] arrayNum2)
+{
+    if (arrayNum1[0] == arrayNum2[0] && arrayNum1[1] == arrayNum2[1])
+    {
+        Console.WriteLine("Прямые совпадают");
+        return false;
+    }
+    else if (arrayNum1[0] == arrayNum2[0] || arrayNum1[1] == arrayNum2[1])
+    {
+        Console.WriteLine("Прямые параллельны");
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+
+}

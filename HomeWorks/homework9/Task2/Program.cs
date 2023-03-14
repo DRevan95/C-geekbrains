9d09a521e8c8ba039887e9bf3cd4bf532e4f67ff
Kirill <kominov.kirill@yandex.ru>
2023-03-14 23:00:29 +0500

    Finish homework9

---
+++ HomeWorks/homework9/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+Console.Clear();
+
+int numbersSum(int M, int N)
+{
+    if (M > N) return 0;
+    return M + N + numbersSum(M + 1, N - 1);
+}
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(numbersSum(M, N));
---
+++ HomeWorks/homework9/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3->A(m, n) = 9
+m = 3, n = 2->A(m, n) = 29
+ */
+
+Console.Clear();
+
+int getAkkermanF(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return getAkkermanF(m - 1, 1);
+    return getAkkermanF(m - 1, getAkkermanF(m, n - 1));
+}
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(getAkkermanF(m, n));
